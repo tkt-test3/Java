b8f227450784253e59a2ec23acc31ae1c24b457f
tkt-test3 <mayuko.takita+git3@findy.co.jp>
2025-08-14 15:36:36 +0900

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,28 @@
+// Product.java
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+               "name='" + name + '\'' +
+               ", price=" + price +
+               '}';
+    }
+}
